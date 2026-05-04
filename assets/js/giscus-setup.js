be17b81fbcde98ca5cd506aba356ec52ce2676b3
haowuqmul-cloud <haowuqmul@gmail.com>
2026-05-04 09:56:45 +0000

    Deploying to gh-pages from @ haowuqmul-cloud/haowuqmul-cloud.github.io@19400eaf6cefe04754a608a274d32e5777f8e262 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "haowuqmul-cloud/hao-wu.github.io",
+    "data-repo": "haowuqmul-cloud/haowuqmul-cloud.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
